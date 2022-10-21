d245e74f2c87bdabfb89316551430fe513ba7518
codedghost2 <codedghost2@gmail.com>
2022-10-21 11:56:06 +0100

    Initial deserialization of webhook payload

--- CodedGhost.PrintfulWebhooksFunction/PrintfulWebhooksFunction.cs
+++ CodedGhost.PrintfulWebhooksFunction/PrintfulWebhooksFunction.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PrintfulLib.Models.WebhookResponses;
 
 namespace CodedGhost.PrintfulWebhooksFunction
 {
@@ -16,9 +19,13 @@ namespace CodedGhost.PrintfulWebhooksFunction
         }
 
         [Function("PrintfulWebhooksFunction")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var webhookContent = await req.Body.DeserializeFromStream<PrintfulWebhookResponse>();
+
+            _logger.LogInformation(JsonConvert.SerializeObject(webhookContent));
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
---
+++ CodedGhost.PrintfulWebhooksFunction/StreamExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace CodedGhost.PrintfulWebhooksFunction;
+
+// Should think about pulling this out to a small library package.
+public static class StreamExtensions
+{
+    public static async Task<T?> DeserializeFromStream<T>(this Stream stream)
+    {
+        var content = await new StreamReader(stream).ReadToEndAsync();
+
+        return JsonConvert.DeserializeObject<T>(content);
+    }
+}
